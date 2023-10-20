85e2777d9980839aee5de840df06b4a6fc105e42
klakho0400 <klakhotia113@gmail.com>
2023-10-20 14:04:02 -0700

    Binary Search Completed

---
+++ Binary Search/Time Based Key Value Store.cpp
@@ -0,0 +1,13 @@
+class TimeMap {
+public:
+unordered_map<string, map<int, string>> m;
+void set(string key, string value, int timestamp) {
+  m[key][timestamp]=value;
+}
+string get(string key, int timestamp) {
+  auto it=m[key].upper_bound(timestamp);
+        if(it==m[key].begin())return "";
+        it--;
+        return it->second;
+}
+};
